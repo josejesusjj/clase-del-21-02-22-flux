55626e34474e9d9416e580b316dc846d8038a83d
Jose <josejesuscedeno@gmail.com>
2022-02-21 18:08:44 +0000

    contact

---
+++ src/js/views/contact.js
@@ -0,0 +1,10 @@
+import React from "react";  
+
+const Contact = () => {
+    <div>
+        <h3>Datos de contacto:</h3>
+        <p>234234234, contacto@email.es</p>
+    </div>
+}
+
+export default Contact
